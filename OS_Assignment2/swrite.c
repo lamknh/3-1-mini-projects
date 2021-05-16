58c640859f709e5edd01499ddc6e107330c46804
Kim Nahyeong <of.kimna@gmail.com>
2021-05-16 10:41:40 +0900

    directory orginized, sread.c swrite.c modified

---
+++ OS_Assignment2/swrite.c
@@ -0,0 +1,22 @@
+#include <linux/kernel.h>
+#include <linux/linkage.h>
+#include <linux/syscalls.h>
+#include <linux/module.h>
+#include <linux/fcntl.h>
+#include <linux/file.h>
+
+int sys_swrite(int fd, char* buf, int len) {
+	int i, n;
+	printk("sys_swrite()\n");
+
+	for (i = 0; i < len; i++) {
+		buf[i] = ~buf[i];
+	}
+
+	n = ksys_write(fd, buf, len);
+	return n;
+}
+
+SYSCALL_DEFINE3(swrite, int, fd, char*, buf, int, len) {
+	return sys_swrite(fd, buf, len);
+}
